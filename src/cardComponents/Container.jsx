df74cf3859f36bfa03b944994e3e52bca32e38db
Th0rnike <116254117+Th0rnike@users.noreply.github.com>
2023-11-13 16:29:21 +0400

    independent commit

--- src/cardComponents/Container.jsx
+++ src/cardComponents/Container.jsx
@@ -1,12 +1,11 @@
 import '../App.css';
-import mainIMG from '../images/image-equilibrium.jpg'
 import Profile from './Profile';
 import Separate from './Separate';
 
 export default function Container(){
     return(
         <div className="container">
-            <img className='mainIMG' src={mainIMG} alt="" />
+            <div className='mainIMG'></div>
             <h1 className='heading'>Equilibrium #3429</h1>
             <p>Our Equilibrium collection promotes balance and calm.</p>
             <Separate />
